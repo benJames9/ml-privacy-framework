2e16baa2969a088f2799f1065a84a84dcca6cfdd
sa2021 <sa2021@ic.ac.uk>
2024-02-07 19:35:31 +0000

    refactor(frontend): move horizontal line to separate component HBar

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,5 @@
 import FileUpload from "@/components/FileUpload";
+import HBar from "@/components/HBar";
 import ModelSelect from "@/components/ModelSelect";
 import Navbar from "@/components/Navbar";
 
@@ -10,9 +11,10 @@ export default function Home() {
       <div className="flex min-h-screen flex-col items-center justify-between px-24 py-8 bg-gradient-to-r from-black to-blue-950">
         <h2 className="text-3xl font-bold text-gray-400 mb-8">Select Model</h2>
         <ModelSelect models={models} />
-        <hr className="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700" />
+        <HBar />
         <h3 className="text-2xl font-bold text-gray-400 mb-8">Upload Model Parameters</h3>
         <FileUpload expectedFileType="pt" label="Select File (.pt)" />
+        <HBar />
       </div>
     </main>
   )
