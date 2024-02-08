83217d069b7425fab9dced6cb0eece808a2eb245
sa2021 <sa2021@ic.ac.uk>
2024-02-08 15:35:09 +0000

    feat(frontend): add evaluate button to setup page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import DatasetParams from "@/components/DatasetParams";
 import FileUpload from "@/components/FileUpload";
 import HBar from "@/components/HBar";
@@ -5,9 +6,15 @@ import ModelSelect from "@/components/ModelSelect";
 import Navbar from "@/components/Navbar";
 import AttackParams from "@/components/AttackParams";
 import LoggingParams from "@/components/LoggingParams";
+import EvaluateButton from "@/components/EvaluateButton";
 
 export default function Home() {
   const models: string[] = ["ResNet-18", "Model 2", "Model 3", "Model 4"];
+
+  const onClick = () => {
+    console.log("Clicked");
+  }
+
   return (
     <main>
       <Navbar />
@@ -26,6 +33,7 @@ export default function Home() {
         <HBar />
         <h3 className="text-2xl font-bold text-gray-400 mb-4">Logging Parameters</h3>
         <LoggingParams />
+        <EvaluateButton onClick={onClick} />
       </div>
     </main>
   )
