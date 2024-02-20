b23c9a96a1fa4503615db410d26ed5fc44858842
sa2021 <sa2021@ic.ac.uk>
2024-02-20 19:39:03 +0000

    feat(error-page): add styling to error page

--- frontend/src/app/server-disconnect/page.tsx
+++ frontend/src/app/server-disconnect/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useSearchParams } from 'next/navigation'
+import Navbar from "@/components/Navbar";
 
 const onClick = async () => {
   window.location.href = `/`;
@@ -11,23 +12,24 @@ const ConnectionClosedPage = () => {
   const error = searchParams.get('error')
 
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center">
-      <h1 className="text-3xl font-bold text-white-600 mb-4">
-        {error}
-      </h1>
-      <div className="flex justify-center">
+    <main>
+      <Navbar />
+      <div className="flex min-h-screen flex-col items-center text-center px-24 py-8 bg-gradient-to-r from-black to-blue-950">
+        <h1 className="text-3xl font-bold text-white-600 mt-16 mb-8">
+          {error || "Unknown Error"}
+        </h1>
         <button
           type="button"
           className={
-            `bg-gray-900 text-white hover:bg-gray-600 border border-gray-300 text-gray-700 font-medium rounded-lg text-center inline-flex items-center px-3 py-2 text-base`
+            `bg-gray-900 text-white hover:bg-gray-600 border border-gray-300 text-gray-700 font-medium rounded-lg text-center inline-flex items-center px-3 py-2 mt-4 text-base`
           }
           onClick={onClick}
         >
           Return to Home Page
         </button>
       </div>
-    </div>
+    </main>
   );
 };
-  
+
 export default ConnectionClosedPage;
