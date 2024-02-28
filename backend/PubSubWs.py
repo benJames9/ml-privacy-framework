393160469b17f4411fe5c12744a9f118f2eafe3e
Ali Abbas <aliabbas.299792@gmail.com>
2024-02-28 13:56:09 +0000

    fix(pubsubws): ensure to await for closing websockets to invalid routes

--- backend/PubSubWs.py
+++ backend/PubSubWs.py
@@ -15,15 +15,15 @@ class PubSubWs:
         self._dict_lock = Lock()
 
     # Setup websocket to accept connections
-    def setup(self, app: FastAPI, base_route: str):        
+    def setup(self, app: FastAPI, base_route: str):
         @app.websocket(f"{base_route}/{{request_token}}")
         async def _websocket_endpoint(ws: WebSocket, request_token: str):
             if request_token not in self._route_dict:
-                self._close_websocket(ws, request_token, "Non-existent route")
+                await self._close_websocket(ws, request_token, "Non-existent route")
                 return
-            
+
             await ws.accept()
-            
+
             # Set timeout for webscket connection
             create_task(self._close_websocket_after_timeout(ws, request_token))
 
@@ -42,14 +42,14 @@ class PubSubWs:
                     await ws.receive()  # Just so we can monitor when it closes
             except Exception as e:
                 await self._close_websocket(ws, request_token, str(e))
-                    
+
     # Function to close the WebSocket after timeout
     async def _close_websocket_after_timeout(self, ws: WebSocket, request_token: str):
         await sleep(WEBSOCKET_TIMEOUT_SECONDS)
-        
+
         # Close the websocket
         await self._close_websocket(ws, request_token, "Websocket Error: Timeout")
-        
+
     # Close websocket and handle routes
     async def _close_websocket(self, ws: WebSocket, request_token: str, error: str):
         # Wrap lock around closure so closing and removing is atomic
@@ -58,18 +58,18 @@ class PubSubWs:
                 error_message = self._generate_error(error)
                 await ws.send_text(json.dumps(error_message))
                 await ws.close()
-            
+
             # Remove from routes dict
             if request_token in self._route_dict:
                 if ws in self._route_dict[request_token]:
                     self._route_dict[request_token].remove(ws)
-    
+
     # Close all websockets for a given token
     async def close_tokens_websockets(self, request_token: str, error: str):
         for ws in self._route_dict[request_token]:
             await self._close_websocket(ws, request_token, error)
-    
-    # Generate JSON error message to send to client       
+
+    # Generate JSON error message to send to client
     def _generate_error(self, error: str):
         return {
             "message_type": "error",
@@ -79,7 +79,7 @@ class PubSubWs:
     # Publish attack responses to clients
     async def publish_serialisable_data(self, request_token: str, data):
         str_data = ""
-        
+
         # Extract string from data
         try:
             str_data = json.dumps(data)
@@ -99,7 +99,7 @@ class PubSubWs:
     async def publish(self, request_token: str, data_str: str):
         if not request_token in self._route_dict:
             raise Exception("Cannot publish to non existent route")
-        
+
         # Cache the last published data
         async with self._dict_lock:
             self._last_published_data[request_token] = data_str
