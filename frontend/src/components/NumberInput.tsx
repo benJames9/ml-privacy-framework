c220c04d8ba178f15049b287f6bba65674d91236
sa2021 <sa2021@ic.ac.uk>
2024-03-04 23:49:38 +0000

    feat(home-page): add min=0 to number input

--- frontend/src/components/NumberInput.tsx
+++ frontend/src/components/NumberInput.tsx
@@ -10,6 +10,7 @@ const NumberInput: React.FC<NumberInputProps> = ({ label, onChange, isRequired =
       <h3 className="font-semibold text-white mr-4 flex items-start whitespace-pre">{label} {isRequired && <span className="text-sm text-red-500">*</span>}</h3>
       <input
         type="number"
+        min="0"
         onChange={onChange}
         onWheel={e => { e.currentTarget.blur() }}
         className="text-black bg-gray-300 border border-gray-500 rounded-md py-2 px-3 focus:ring-blue-500 focus:border-blue-500 sm:text-sm" />
