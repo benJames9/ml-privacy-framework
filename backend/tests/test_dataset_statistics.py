9f4b4fbc585a1f2ceb10278c7d191513f372a17a
Ben James <bj420@ic.ac.uk>
2024-03-05 12:14:17 +0000

    test(test_dataset_statistics.py): add image shape check to tests

--- backend/tests/test_dataset_statistics.py
+++ backend/tests/test_dataset_statistics.py
@@ -15,7 +15,8 @@ class TestDatasetStatistics(unittest.TestCase):
         self.assertEqual(statistics.num_images, 350)
         self.assertEqual(statistics.num_classes, 7)
         self.assertTrue(np.allclose(statistics.mean, [0.30655779, 0.43363997, 0.4549895]))
-        self.assertTrue(np.allclose(statistics.std, [0.18246345 0.19011785 0.18673244]))    
+        self.assertTrue(np.allclose(statistics.std, [0.18246345, 0.19011785, 0.18673244]))
+        self.assertTrue(np.allclose(statistics.image_shape, [375, 500, 3]))
         
 if __name__ == '__main__':
     unittest.main()
