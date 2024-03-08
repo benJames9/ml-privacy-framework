ca89cdf9ee8cb069dd71176f3e1c0361947ff9f8
sa2021 <sa2021@ic.ac.uk>
2024-03-08 04:21:31 +0000

    feat(AttackLoading.tsx): add new loading icon and header

--- frontend/src/components/LoadingIcon.tsx
+++ frontend/src/components/LoadingIcon.tsx
@@ -5,7 +5,7 @@ interface LoadingIconProps {
 const LoadingIcon: React.FC<LoadingIconProps> = ({ size, borderSize}) => {
   return (
     <div className="relative" id="loading-icon">
-      <div className={`h-12 w-${size} rounded-full border-t-${borderSize} border-b-${borderSize} border-gray-200`}></div>
+      <div className={`h-${size} w-${size} rounded-full border-t-${borderSize} border-b-${borderSize} border-gray-200`}></div>
       <div className={`absolute top-0 left-0 h-${size} w-${size} rounded-full border-t-${borderSize} border-b-${borderSize} border-blue-500 animate-spin`}>
       </div>
     </div>
